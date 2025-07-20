f3cab83612dd55ec4ac6060ec11d88e4aa8a9400
simonkisela <kisela.alex@gmail.com>
2025-07-20 14:21:19 +0200

    Zmena

--- barber_backend/db.py
+++ barber_backend/db.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # Načítanie DATABASE_URL z prostredia (Render)
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv(
+    'postgresql://barber_user_user:yhjjZX3EmkYByIKP7RhDvLvVw7j6puZN@dpg-d1ubo9er433s73ej0mpg-a.frankfurt-postgres.render.com/barber_user')
 
 # Pre SQLAlchemy
 engine = create_engine(DATABASE_URL, echo=True)
