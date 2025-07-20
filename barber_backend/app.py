bbc3db5451801889146fba528b24dd54099410f9
simonkisela <kisela.alex@gmail.com>
2025-07-20 10:31:54 +0200

    Zmeny

--- barber_backend/app.py
+++ barber_backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
-from models import init_db
+from barber_backend.models import init_db
 
 app = Flask(__name__)
 CORS(app)
