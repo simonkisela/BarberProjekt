3b6417aabff5c8ca2db8b49ae7a41b2363e1a797
simonkisela <kisela.alex@gmail.com>
2025-07-20 14:22:48 +0200

    Zmena

--- barber_backend/db.py
+++ barber_backend/db.py
@@ -3,14 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Načítanie DATABASE_URL z prostredia (Render)
-DATABASE_URL = os.getenv(
-    'postgresql://barber_user_user:yhjjZX3EmkYByIKP7RhDvLvVw7j6puZN@dpg-d1ubo9er433s73ej0mpg-a.frankfurt-postgres.render.com/barber_user')
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Pre SQLAlchemy
+if DATABASE_URL is None:
+    raise Exception("DATABASE_URL environment variable not set")
+
 engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Základ pre modely
 Base = declarative_base()
